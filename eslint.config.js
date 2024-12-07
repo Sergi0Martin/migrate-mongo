4e743b70ee1da88760b8faf8ba900673cd033f82
Sergio <16337833+Sergi0Martin@users.noreply.github.com>
2024-12-07 12:36:46 +0100

    wip lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,11 @@
-module.exports = [
-  {
-    // files: ["*.ts"],
-    rules: {
-      semi: ['error'],
-    },
+module.exports = {
+  parser: '@babel/eslint-parser',
+  files: ['src/**/*.{js,ts,jsx,tsx}'],
+  rules: {
+    semi: ['error'],
+    'no-unused-vars': 'warn',
   },
-];
+  linterOptions: {
+    reportUnusedDisableDirectives: 'error',
+  },
+};
